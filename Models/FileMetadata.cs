9786926385dcd622451713f751f28235cad67a99
arercon <jannekschroeter@gmail.com>
2025-03-25 02:07:34 +0100

    Model added

---
+++ Models/FileMetadata.cs
@@ -0,0 +1,12 @@
+﻿namespace FileLab.Models
+{
+    public class FileMetadata
+    {
+        public string FileName { get; set; }
+        public string FilePath { get; set; }
+        public long FileSize { get; set; }
+        public DateTime UploadDate { get; set; }
+        public string ContentType { get; set; }
+
+    }
+}
